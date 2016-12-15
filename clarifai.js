bae7fd58c7c99b9065e9eb8505e1d06f287bdbd3
Hannan Ali <hannan@qubit.com>
2016-12-15 23:37:58 +0500

    add server side implementation for cat implementation

---
+++ clarifai.js
@@ -0,0 +1,46 @@
+/**
+ * clarifai.js
+ *
+ * Clarifai is the best AI based Image And Visual Recognition API
+ * which helps in all kinds of tasks based on Visuals.
+ * We are going to use Clarifai's API in order to check images for certain using
+ * their General Model
+ *
+ * Check out Clarifai's demo on https://clarifai.com/demo
+ * Check out Clarifai's v2 Quick Start guide here https://developer-preview.clarifai.com/quick-start/
+ * 
+ * Clarifai offers free image recognition upto 5000 images/month
+ * OMG! OMG! OMG! Awesome!
+ */
+const Clarifai = require('clarifai')
+const config = require('config')
+
+const app = new Clarifai.App(
+    config.clarifai.clientId,
+    config.clarifai.clientSecret
+)
+
+const conceptsForCat = ['cat']
+
+/**
+ * identifyCatImage
+ * identifies if image contains cat
+ * 
+ * @param {String} - url of the required image
+ * @param {Boolean} - `true` if the cat in image is identified :clap:
+ * 
+ * TBD
+ */
+function identifyCatImage (url) {
+    // Call the Clarifai API here for the image
+    // TBD
+    return app.models.predict(Clarifai.GENERAL_MODEL, url)
+        .then(function (response) {
+            // Storing the Array of concepts from the response in concepts
+            const concepts = response.data.outputs[0].data.concepts
+
+            return concepts.some((concept) => conceptsForCat.indexOf(concept.name) !== -1) 
+        })
+}
+
+module.exports = identifyCatImage
---
+++ pusher.js
@@ -0,0 +1,22 @@
+/**
+ * pusher.js
+ * We are using powers of Pusher for the real time functionality
+ * With it's easy to use API, Pusher makes using Real time features
+ * of way really easy.
+ *
+ * All the communications between the server and clients is handled by Pusher,
+ * We just trigger an event and leave the event handling to the Pusher
+ * 
+ * Go to pusher.com and Signup for free
+ */
+
+const Pusher = require('pusher')
+const pusherConfig = require('config').pusher
+
+const pusher = new Pusher({
+  appId: pusherConfig.appId,
+  key: pusherConfig.appKey,
+  secret: pusherConfig.appSecret
+})
+
+module.exports = pusher
